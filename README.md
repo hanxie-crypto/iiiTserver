e2472e01814504ee4c365a53d72dc9a4754c5cdf
wq123456 <aitongbian123@163.com>
2016-08-29 21:39:03 +0800

    修改配置问阿金

--- README.md
+++ README.md
@@ -13,9 +13,9 @@
 
 
 ###特性
-主要测试node 6.0对es6的支持情况，借助es6的数组对象扩展，实现了可扩展路由routes.index.js。
+使用node 6.0,和大量es6的语法，借助es6的数组对象扩展，实现了可扩展路由routes.index.js。
 数据库方面没有使用odm，orm类的库，使用原生的sql语句。
-
+加入了swagger生成的接口文档，可提供在线数据测试
 ###原理说明
 
 
@@ -27,10 +27,13 @@ git clone
 
 
 ###使用方法
+请提前安装 mysql数据库5.5版本
+redis,申请七牛服务。
+
 cd proj
 npm install
 npm start
-浏览器访问 http://127.0.0.1:3000/users
+浏览器访问 http://127.0.0.1:3000/static/index.html
 
 测试
 npm test
--- config.js
+++ config.js
@@ -5,19 +5,19 @@ let config = {};
 const proconfig = {
     privatekey: 'ssss',
     mysql: {
-        host: '115.28.8.74',
+        host: 'localhost',
         port: '3306',
         database: 'ihb',
         charset: 'utf8_general_ci',
-        user: 'root',
-        password: 'wq123',
+        user: 'youraccount',
+        password: 'yourpassword',
         multipleStatements: true,
     },
     listenport: 3000,
     redis_db: 0,
     redis_host: '127.0.0.1',
     redis_port: 6379,
-    session_secret: 'ihb',
+    session_secret: 'yourxxx',
     loggers: {
         infoFile: {
             name: 'info',
@@ -38,35 +38,32 @@ const proconfig = {
     pageCount: 20,
     validator: true,
     qiniu: {
-        ACCESS_KEY:'9Av-wBLloTzjXb0DN7B2BEbilEgMtC9Gfq0fhfZ6',
-        SECRET_KEY: 'JrKY-Lnb7PDGvdzH6ALY6lb7pRMJkeoztxcCytWR',
+        ACCESS_KEY:'yourxxx',
+        SECRET_KEY: 'yourxxx',
     },
-    AppID: 'wx6152431428fe7bdd',
-    //AppSecret: '02d1d1ca9778bd6dae98a9023c62ee9d',
-    AppSecret: '8dd155189431d23d9dfc66689c9369f0',
-    //MchId: '1220364301',
-    MchId: '1277016001',
+    AppID: 'yourxxx',
+    AppSecret: 'yourxxx',
+    MchId: 'yourxxx',
     wechatAccesTokenUrl: 'https://api.weixin.qq.com/cgi-bin/token',
     wechatJsapiTicket: 'https://api.weixin.qq.com/cgi-bin/ticket/getticket',
     userAccessToken: 'https://api.weixin.qq.com/sns/oauth2/access_token',
     commonPayUrl: 'https://api.mch.weixin.qq.com/pay/unifiedorder',
     getUserInfo: 'https://api.weixin.qq.com/sns/userinfo',
-    //KEY: 'PUTAOYUNABCDEFGHIJKLMNOPQRSTUVWX',
-    KEY: 'PUTAOYUNABCDEFGHIJKLMNOPQRSTUVW1',
-    PAY_SUCCESS_URL: 'http://wap.putaoyun.com/wap/paysuccess',
-    SERVER_IP: '114.80.154.68',
+
+    KEY: 'yourxxx',
+
 };
 
 
 const devconfig = {
     privatekey: 'ssss',
     mysql: {
-        host: '115.28.8.74',
+        host: 'localhost',
         port: '3306',
         database: 'ihb',
         charset: 'utf8_general_ci',
-        user: 'root',
-        password: 'wq123',
+        user: 'yourxxx',
+        password: 'yourxxx',
         multipleStatements: true,
     },
     listenport: 3000,
@@ -94,26 +91,24 @@ const devconfig = {
     pageCount: 10,
     validator: true,
     qiniu: {
-        ACCESS_KEY:'9Av-wBLloTzjXb0DN7B2BEbilEgMtC9Gfq0fhfZ6',
-        SECRET_KEY: 'JrKY-Lnb7PDGvdzH6ALY6lb7pRMJkeoztxcCytWR',
+        ACCESS_KEY:'yourxxx',
+        SECRET_KEY: 'yourxxx',
     },
-    AppID: 'wx6152431428fe7bdd',
-    AppSecret: '8dd155189431d23d9dfc66689c9369f0',
-    MchId: '1277016001',
+    AppID: 'yourxxx',
+    AppSecret: 'yourxxx',
+    MchId: 'yourxxx',
     wechatAccesTokenUrl: 'https://api.weixin.qq.com/cgi-bin/token',
     wechatJsapiTicket: 'https://api.weixin.qq.com/cgi-bin/ticket/getticket',
     userAccessToken: 'https://api.weixin.qq.com/sns/oauth2/access_token',
     commonPayUrl: 'https://api.mch.weixin.qq.com/pay/unifiedorder',
     getUserInfo: 'https://api.weixin.qq.com/sns/userinfo',
-    //KEY: 'PUTAOYUNABCDEFGHIJKLMNOPQRSTUVWX',
     KEY: 'PUTAOYUNABCDEFGHIJKLMNOPQRSTUVW1',
     PAY_SUCCESS_URL: 'http://wap.putaoyun.com/wap/paysuccess',
-    SERVER_IP: '114.80.154.68',
 }
 if (process.env.NODE_ENV !== 'production') {
     config = devconfig;
 } else {
     config = proconfig;
 }
-config.qiniuurl = 'http://7xuwbb.com1.z0.glb.clouddn.com/';
+config.qiniuurl = 'yourxxx'; //七牛云的基础地址，用来返回上传后生成的文件地址
 module.exports = config;
